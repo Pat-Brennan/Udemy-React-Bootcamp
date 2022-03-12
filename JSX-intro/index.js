029fc300815f09bc97dcd40fac36f527230a6390
Patrick Brennan <patbrennan7@gmail.com>
2022-03-12 17:32:58 -0500

    JSX Intro Started

---
+++ JSX-intro/index.js
@@ -0,0 +1,6 @@
+//* Intro to JSX
+//? Stands for JavaScript Extension, JS + XML
+//? Allows us to write our JS, HTML, and CSS together
+//? Combines writing your UI, and logic for that UI TOGETHER
+//? It's then compiled by BABEL into regular JS
+
