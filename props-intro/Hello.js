2335362e9770e76e0c73e68a13b1a04840d02734
Patrick Brennan <patbrennan7@gmail.com>
2022-03-14 17:06:24 -0400

    Prop Types Discussed

--- props-intro/Hello.js
+++ props-intro/Hello.js
@@ -1,5 +1,18 @@
 class Hello extends React.Component {
-    render() {
-        return <h1>To:??? From:??? </h1>
-    }
+	render() {
+		console.log(this.props); //Verify we are getting props with console
+
+		let bangs = '!'.repeat(this.props.bangs);
+		return (
+			<div>
+				<h1>
+					To: {this.props.to} From: {this.props.from}
+					{this.props.num} {this.props.data} {bangs}
+				</h1>
+				{/* the image url is brought in as a string in PROPS */}
+				{/* then displayed using the IMAGE TAG */}
+				<img src={this.props.img}></img>
+			</div>
+		);
+	}
 }
--- props-intro/index.js
+++ props-intro/index.js
@@ -1,13 +1,35 @@
 //* Props (Properties)
 //? A useful component is a reusable one
 //? This often means making it configurable or customizable!
+//! Props are NOT MUTABLE!. Something like this.props.to = "Johnny" will NOT WORK
 
+//? Props can be Strings:
+//? <User name="Jane" title="CEO" />
+
+//? but for other types, we embed JS expressions with {}
+//? <User name="Jane" salary={ 100000 } hobbies={["Being weird", "Doing stuff", "Makin things"]} />
 class App extends React.Component {
-    render() {
-        return (
-            <Hello />
-        )
-    }
+	render() {
+		return (
+			<div>
+				{/* These are Props! (to="" from="")*/}
+				{/* They make it very to pass info from a PARENT comp to a CHILD comp*/}
+				<Hello
+					to='Ringo'
+					from='Paul'
+					num={7}
+					data={[1, 2, 3, 4, 5]}
+					isFunny={false}
+				/>
+				<Hello
+					to='George'
+					from='John'
+					bangs={10}
+					img='https://images.unsplash.com/photo-1597582797808-6a4162708a4b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGRvZ2dvfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+				/>
+			</div>
+		);
+	}
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
