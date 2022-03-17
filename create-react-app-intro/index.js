5ba6d9f0b34eed34ce88bb44605237423617aa01
Patrick Brennan <patbrennan7@gmail.com>
2022-03-17 14:29:01 -0400

    React App Created

---
+++ create-react-app-intro/index.js
@@ -0,0 +1,11 @@
+//* Create-React-App
+//? React is a front-end library - you don't need server-side stuff!
+//? Technically, you can get react.js and react-dom.js from a CDN
+//? Technically, you can transpile JSX in the browser at runtime.
+//! But there's a better way!
+
+//? Create-React-App is a utility script that:
+//? - Creates a skeleton react project
+//? - Sets it up so that JS files are fun through Babel automatically
+//? - Lets us use super-modern JavaScript features/idioms
+//? - Makes testing & deployment much easier
