db61a9478cb1204815e0490e0b9aa6d322480a6c
Patrick Brennan <patbrennan7@gmail.com>
2023-01-06 15:13:34 -0500

    Convert all class comps to function comps

--- react-app-structure/Hello.js
+++ react-app-structure/Hello.js
@@ -1,8 +1,12 @@
 //? It's best practice for each component to be IT'S OWN FILE
 //? This enforces the "Single Responsibility" Principle
 
-class Hello extends React.Component {
-	render() {
-		return <h1>Hey there!</h1>;
-	}
+// class Hello extends React.Component {
+// 	render() {
+// 		return <h1>Hey there!</h1>;
+// 	}
+// }
+
+function Hello() {
+	return <h1> HI!!! </h1>;
 }
