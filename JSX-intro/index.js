ef47da5619b5ce0f2edbd3ee1ce7fc3928461c35
Patrick Brennan <patbrennan7@gmail.com>
2023-01-06 14:50:08 -0500

    Learn something new everyday

--- JSX-intro/index.js
+++ JSX-intro/index.js
@@ -16,8 +16,8 @@ function getMood() {
 }
 
 const silly = () => {
-	console.log("Silly")
-}
+	console.log('Silly');
+};
 
 silly();
 class JSXDemo extends React.Component {
@@ -38,23 +38,36 @@ function getNum() {
 	return Math.floor(Math.random() * 10) + 1;
 }
 
-class NumPicker extends React.Component {
-    render() {
-        const num = getNum()
-		return (
-			<div>
-                <h1>Your number is... {num}!</h1>
-                <p>{num === 7 ? 'Congrats!' : 'Unlucky!'}</p>
-                {
-                    num === 7
-                    ? <img src='https://media2.giphy.com/media/S3We8KS67EbjID2H85/200w.webp?cid=ecf05e4788oez0v2i5h21udzq6zzkzap7iayxu9ynz25f1bi&rid=200w.webp&ct=g'></img>
-                    : null
-                }
-			</div>
-		);
-	}
-}
+// class NumPicker extends React.Component {
+//     render() {
+//         const num = getNum()
+// 		return (
+// 			<div>
+//                 <h1>Your number is... {num}!</h1>
+//                 <p>{num === 7 ? 'Congrats!' : 'Unlucky!'}</p>
+//                 {
+//                     num === 7
+//                     ? <img src='https://media2.giphy.com/media/S3We8KS67EbjID2H85/200w.webp?cid=ecf05e4788oez0v2i5h21udzq6zzkzap7iayxu9ynz25f1bi&rid=200w.webp&ct=g'></img>
+//                     : null
+//                 }
+// 			</div>
+// 		);
+// 	}
+// }
 
+//? Same as above but with a functional component
+function NumPicker() {
+	const num = getNum();
+	return (
+		<div>
+			<h1>Your number is... {num}!</h1>
+			<p>{num === 7 ? 'Congrats!' : 'Unlucky!'}</p>
+			{num === 7 ? (
+				<img src='https://media2.giphy.com/media/S3We8KS67EbjID2H85/200w.webp?cid=ecf05e4788oez0v2i5h21udzq6zzkzap7iayxu9ynz25f1bi&rid=200w.webp&ct=g'></img>
+			) : null}
+		</div>
+	);
+}
 
 ReactDOM.render(<JSXDemo />, document.getElementById('root'));
 ReactDOM.render(<NumPicker />, document.getElementById('root2'));
