bdf3e1d3fa9968ebde5f630d3a480340e1cdb1c1
Patrick Brennan <patbrennan7@gmail.com>
2023-01-08 17:35:10 -0500

    Files Updated

--- props-intro/Hello.js
+++ props-intro/Hello.js
@@ -1,6 +1,6 @@
 class Hello extends React.Component {
 	render() {
-		console.log(this.props); //Verify we are getting props with console
+		console.log(this.props); // Verify we are getting props with console
 
 		let bangs = '!'.repeat(this.props.bangs);
 		return (
@@ -16,3 +16,18 @@ class Hello extends React.Component {
 		);
 	}
 }
+
+// function Hello() {
+// 	let bangs = '!'.repeat(this.props.bangs)
+// 	return (
+// 		<div>
+// 		<h1>
+// 			To: {this.props.to} From: {this.props.from}
+// 			{this.props.num} {this.props.data} {bangs}
+// 		</h1>
+// 		{/* the image url is brought in as a string in PROPS */}
+// 		{/* then displayed using the IMAGE TAG */}
+// 		<img src={this.props.img}></img>
+// 	</div>
+// 	)
+// }
