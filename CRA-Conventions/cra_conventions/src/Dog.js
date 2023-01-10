a5fe0e0789a8b627cc24e739b2dd172ea79c0a18
Patrick Brennan <patbrennan7@gmail.com>
2023-01-10 14:37:51 -0500

    CSS Conventions Covered

--- CRA-Conventions/cra_conventions/src/Dog.js
+++ CRA-Conventions/cra_conventions/src/Dog.js
@@ -1,6 +1,15 @@
+import './Dog.css';
+import Eorah from './Eorah.jpg'
+
 //? Always keep the component and file name the same!
 function Dog() {
-  console.log("Hello! I am a Dog!")
+	return (
+		<div className="Dog">
+      <h1> I am a dog!!! </h1>
+      <img className="Dog-img" src={ Eorah } alt="Actually a cat ^_^" />
+		</div>
+	);
 }
-//? Keep it to one component per file, and export default when possible! 
+
+//? Keep it to one component per file, and export default when possible!
 export default Dog;
