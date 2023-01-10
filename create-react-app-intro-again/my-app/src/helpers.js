1c569e70fe3ab4a2a0b6f992bb1592edb8093a61
Patrick Brennan <patbrennan7@gmail.com>
2023-01-09 20:38:19 -0500

    Left off at Fruits Exercise

---
+++ create-react-app-intro-again/my-app/src/helpers.js
@@ -0,0 +1,20 @@
+
+
+function Helpers() {
+  console.log('BEHOLD! My Helpfulness!');
+}
+
+function Sort() {
+  console.log('SORTED!')
+}
+
+function Sing() {
+  console.log('TRA LA LA!')
+}
+
+// export default Helpers
+export {
+  Helpers,
+  Sort,
+  Sing
+}
