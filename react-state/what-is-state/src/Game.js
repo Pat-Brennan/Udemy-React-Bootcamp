45e88fc1dda333647dfffb091931786155873da1
Patrick Brennan <patbrennan7@gmail.com>
2023-01-16 17:07:48 -0500

    Basics of State Covered

---
+++ react-state/what-is-state/src/Game.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      score: 0,
+      gameOver: false
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1> Your Score is: { this.state.score } </h1>
+      </div>
+    )
+  }
+}
+
+export default Game;
+
+//* Alternate Syntax for State
+// state = {
+//   score: 666,
+//   gameOver: false
+// }
+
+//? As you can see, there is no constructor.
+//! Babel is converting this to be functional JavaScript
+//? Good when using create-react-app, not good in a vanilla JS scenario
