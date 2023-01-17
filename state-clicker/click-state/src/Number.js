878a4ce4b42cb8f3125886c748b8dc7192d4523a
Patrick Brennan <patbrennan7@gmail.com>
2023-01-17 16:56:31 -0500

    Winner Functionality Added

--- state-clicker/click-state/src/Number.js
+++ state-clicker/click-state/src/Number.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
 
-class Number extends Component{
-  constructor(props) {
-    super(props);
-  }
-  rando() {
-    return Math.floor(Math.random() * 10)
-  }
-  render() {
-    return (
-      <div>
-        <h1>Number is { this.rando() } </h1>
-        <button type="Submit"> RANDOMIZE! </button>
-      </div>
-    )
-  }
+class Number extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			num: 1,
+		};
+		this.rando = this.rando.bind(this);
+	}
+
+	//? Generates a Random Number
+	rando() {
+		let random = Math.floor(Math.random() * 10) + 1;
+		this.setState({ num: random });
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Number is: {this.state.num} </h1>
+				{this.state.num === 7 ? (
+					<h2> WE'RE SO PROUD OF YOU! </h2>
+				) : (
+					<button type='Submit' onClick={this.rando}>
+						RANDOMIZE!
+					</button>
+				)}
+			</div>
+		);
+	}
 }
 
 export default Number;
