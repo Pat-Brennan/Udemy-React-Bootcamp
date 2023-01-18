c45b31341ec2b074c22d7ea7f9581d930f755a2f
Patrick Brennan <patbrennan7@gmail.com>
2023-01-18 14:41:12 -0500

    Dice Project Started

---
+++ Dice-Roller/dice-roller/src/RollDice.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class RollDice extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <h1> AM I ALIVE? 👻 </h1>
+      </div>
+    )
+  }
+}
+
+export default RollDice;
