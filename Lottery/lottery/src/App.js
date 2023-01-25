c36a1da2c51d26f0900255bf84c898404c7e5ce8
Patrick Brennan <patbrennan7@gmail.com>
2023-01-25 15:20:56 -0500

    Lottery App Started

---
+++ Lottery/lottery/src/App.js
@@ -0,0 +1,12 @@
+import Lottery from './Lottery'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Lottery />
+    </div>
+  );
+}
+
+export default App;
---
+++ Lottery/lottery/src/Lottery.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Lottery extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+      <div>
+        <h1> Am I Alive? 💀 </h1>
+      </div>
+    )
+  }
+}
+
+export default Lottery;
